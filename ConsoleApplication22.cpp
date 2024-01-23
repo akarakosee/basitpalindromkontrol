c561d0ff6dfa36bd8331a8f5d37da5e35700f890
akarakosee <132479583+akarakosee@users.noreply.github.com>
2024-01-23 15:40:55 +0300

    Add files via upload

---
+++ ConsoleApplication22.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+bool palindrom(string mu) {
+    // Muhtemel boşlukları ve büyük harfleri kaldır
+    mu.erase(remove(mu.begin(), mu.end(), ' '), mu.end());
+    (mu.begin(), mu.end(), mu.begin(), ::tolower);
+    // Mu'nun uzunluğunu bul
+    int n = mu.length();
+    // Mu'nun ilk yarısını al
+    string ilk = mu.substr(0, n / 2);
+    // Mu'nun ikinci yarısını al ve ters çevir
+    string ikinci = mu.substr(n / 2 + n % 2, n / 2);
+    reverse(ikinci.begin(), ikinci.end());
+    // İlk ve ikinci yarıyı karşılaştır
+    if (ilk == ikinci) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+// Örnek kullanım
+int main() {
+    cout << palindrom("kayak") << endl; // 1
+    cout << palindrom("araba") << endl; // 0
+}
